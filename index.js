4bf0632baa1acb3777ce94d26bd93a7b8cdd58b7
KUNszg <50715419+kunszg@users.noreply.github.com>
2022-12-27 22:15:38 +0100

    add rabbitmq queue system, fix responses, change functional logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./config");
+const amqplib = require('amqplib');
+const {Player} = require("discord-music-player");
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"]
@@ -9,8 +11,6 @@ const settings = {
     prefix: '%',
     token: config.token
 };
-
-const {Player, RepeatMode} = require("discord-music-player");
 
 const player = new Player(client, {
     leaveOnEnd: false,
@@ -23,137 +23,216 @@ const player = new Player(client, {
 
 client.player = player;
 
+const queue = 'KBOT.MUSIC_PLAYER';
+
+const opt = {credentials: require('amqplib').credentials.plain(config.rabbitUser, config.rabbitPass)};
+
+const conn = await amqplib.connect(config.rabbitUrl, opt);
+
+const rabbitConsumer = await conn.createChannel();
+await rabbitConsumer.assertQueue(queue);
+
 let songData = [];
-const playerAction = async () => {
-    const {RepeatMode} = require('discord-music-player');
 
-    client.on('messageCreate', async (message) => {
-        const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-        const command = args.shift();
-        let guildQueue = client.player.getQueue(message.guild.id);
+(async () => {
+    // Rabbit listener
+    // rabbitConsumer.consume(queue, (msg) => {
+    //     if (msg !== null) {
+    //         console.log('Recieved:', msg.content.toString());
+    //         rabbitConsumer.ack(msg);
+    //     } else {
+    //         console.log('Consumer cancelled by server');
+    //     }
+    // });
 
-        if (!message.member || !message.guild) {
-            return;
-        }
+    // Rabbit sender
+    const publishMessage = await conn.createChannel();
 
-        const Guild = client.guilds.cache.get(message.guild.id); // Getting the guild.
-        const Member = Guild.members.cache.get(message.member.id); // Getting the member.
+    const playerAction = async () => {
+        const {RepeatMode} = require('discord-music-player');
 
-        if (command === 'play') {
-            if (!Member.voice.channel) {
-                await message.reply("You are not in a voice channel!");
+        client.on('messageCreate', async (message) => {
+            const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+            const command = args.shift();
+            let guildQueue = client.player.getQueue(message.guild.id);
 
+            if (!message.member || !message.guild) {
                 return;
             }
 
-            let queue = client.player.createQueue(message.guild.id);
+            const Guild = client.guilds.cache.get(message.guild.id); // Getting the guild.
+            const Member = Guild.members.cache.get(message.member.id); // Getting the member.
 
-            await queue.join(message.member.voice.channel);
+            if (command === 'play') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-            let song = await queue.play(args.join(' ')).catch(err => {
-                console.log(err);
-                if (!guildQueue)
-                    queue.stop();
-            });
+                let queue = client.player.createQueue(message.guild.id);
 
-            songData.push({
-                guildObject: Guild,
-                memberObject: Member,
-                guild: message.guild.id,
-                channel: message.channel.id,
-                songData: song
-            });
-        }
+                await queue.join(message.member.voice.channel);
 
-        if (command === 'playlist') {
-            let queue = client.player.createQueue(message.guild.id);
-            await queue.join(message.member.voice.channel);
-            let song = await queue.playlist(args.join(' ')).catch(err => {
-                console.log(err);
-                if (!guildQueue)
-                    queue.stop();
-            });
-        }
+                let song = await queue.play(args.join(' ')).catch(err => {
+                    console.log(err);
+                    if (!guildQueue)
+                        queue.stop();
+                });
 
-        if (command === 'skip') {
-            if (!Member.voice.channel) {
-                await message.reply("You are not in a voice channel!");
+                const songObject = {
+                    guildObject: Guild,
+                    memberObject: Member,
+                    guild: message.guild.id,
+                    channel: message.channel.id,
+                    songData: song
+                };
 
-                return;
+                songData.push(songObject);
+
+                publishMessage.sendToQueue(queue, Buffer.from(JSON.stringify(songObject)));
             }
 
-            guildQueue.skip();
-        }
+            if (command === 'playlist') {
+                let queue = client.player.createQueue(message.guild.id);
+                await queue.join(message.member.voice.channel);
+                let song = await queue.playlist(args.join(' ')).catch(err => {
+                    console.log(err);
+                    if (!guildQueue)
+                        queue.stop();
+                });
+            }
 
-        if (command === 'stop') {
-            guildQueue.stop();
-        }
+            if (command === 'skip') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
 
-        if (command === 'removeLoop') {
-            guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
-        }
+                    return;
+                }
 
-        if (command === 'toggleLoop') {
-            guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
-        }
+                guildQueue.skip();
+            }
 
-        if (command === 'toggleQueueLoop') {
-            guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
-        }
+            if (command === 'stop') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'setVolume') {
-            guildQueue.setVolume(parseInt(args[0]));
-        }
+                guildQueue.stop();
+            }
 
-        if (command === 'seek') {
-            guildQueue.seek(parseInt(args[0]) * 1000);
-        }
+            if (command === 'removeLoop') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'clearQueue') {
-            guildQueue.clearQueue();
-        }
+                guildQueue.setRepeatMode(RepeatMode.DISABLED); // or 0 instead of RepeatMode.DISABLED
+            }
 
-        if (command === 'shuffle') {
-            guildQueue.shuffle();
-        }
+            if (command === 'toggleLoop') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'getQueue') {
-            console.log(guildQueue);
-        }
+                guildQueue.setRepeatMode(RepeatMode.SONG); // or 1 instead of RepeatMode.SONG
+            }
 
-        if (command === 'getVolume') {
-            console.log(guildQueue.volume)
-        }
+            if (command === 'toggleQueueLoop') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'nowPlaying') {
-            console.log(`Now playing: ${guildQueue.nowPlaying}`);
-        }
+                guildQueue.setRepeatMode(RepeatMode.QUEUE); // or 2 instead of RepeatMode.QUEUE
+            }
 
-        if (command === 'pause') {
-            guildQueue.setPaused(true);
-        }
+            if (command === 'setVolume') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'resume') {
-            guildQueue.setPaused(false);
-        }
+                guildQueue.setVolume(parseInt(args[0]));
+            }
 
-        if (command === 'remove') {
-            guildQueue.remove(parseInt(args[0]));
-        }
+            if (command === 'seek') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-        if (command === 'createProgressBar') {
-            const ProgressBar = guildQueue.createProgressBar();
+                guildQueue.seek(parseInt(args[0]) * 1000);
+            }
 
-            // [======>              ][00:35/2:20]
-            console.log(ProgressBar.prettier);
-        }
-    })
-}
+            if (command === 'clearQueue') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
 
-client.on("ready", async () => {
-    await playerAction();
+                guildQueue.clearQueue();
+            }
 
-    client.player.on('songFirst', (queue, song) => {
+            if (command === 'shuffle') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
+
+                guildQueue.shuffle();
+            }
+
+            if (command === 'getQueue') {
+                console.log(guildQueue);
+            }
+
+            if (command === 'getVolume') {
+                console.log(guildQueue.volume)
+            }
+
+            if (command === 'nowPlaying') {
+                console.log(`Now playing: ${guildQueue.nowPlaying}`);
+            }
+
+            if (command === 'pause') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
+
+                guildQueue.setPaused(true);
+            }
+
+            if (command === 'resume') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
+
+                guildQueue.setPaused(false);
+            }
+
+            if (command === 'remove') {
+                if (!Member.voice.channel) {
+                    await message.reply("You are not in a voice channel!");
+                    return;
+                }
+
+                guildQueue.remove(parseInt(args[0]));
+            }
+
+            if (command === 'createProgressBar') {
+                const ProgressBar = guildQueue.createProgressBar();
+
+                // [======>              ][00:35/2:20]
+                console.log(ProgressBar.prettier);
+            }
+        })
+    }
+
+    const sendMessage = (queue, song) => {
         if (!song.name) {
             return;
         }
@@ -163,31 +242,29 @@ client.on("ready", async () => {
         if (getSong) {
             const channel = client.channels.cache.get(getSong.channel);
 
-            channel.send(`\`\`\`▶️ Now playing: \n\n Song: ${song.name} \n\n Autor: ${song.author} \n \n ⌛ ${song.duration}min\`\`\`  \n requested by <@${getSong.memberObject.id}>`);
+            channel.send(`\`\`\`▶️ Now playing: \n\n Song: ${song.name} \n\n Author: ${song.author} \n \n ⌛ ${song.duration} min\`\`\`  \n requested by <@${getSong.memberObject.id}>`);
 
             songData = songData.filter(function (el) {
                 return el.songData.name !== song.name;
             });
         }
-    })
+    }
 
-    client.player.on('songChanged', (queue, songNew) => {
-        if (!songNew.name) {
-            return;
-        }
+    client.on("ready", async () => {
+        await playerAction();
 
-        const getSong = songData.find(i => i.songData?.name ?? null === songNew.name);
+        client.player.on('songFirst', (queue, song) => {
+            sendMessage(queue, song);
+        });
 
-        if (getSong) {
-            const channel = client.channels.cache.get(getSong.channel);
+        client.player.on('songChanged', (queue, song) => {
+            sendMessage(queue, song);
+        });
 
-            channel.send(`\`\`\`▶️ Now playing: \n\n Song: ${songNew.name} \n\n Autor: ${songNew.author} \n \n ⌛ ${songNew.duration}min\`\`\`  \n requested by <@${getSong.memberObject.id}>`);
+        client.player.on('songAdd', (queue, song) => {
+            sendMessage(queue, song);
+        });
+    });
 
-            songData = songData.filter(function (el) {
-                return el.songData.name !== songNew.name;
-            });
-        }
-    })
-});
-
-client.login(settings.token);
+    client.login(settings.token);
+})();
